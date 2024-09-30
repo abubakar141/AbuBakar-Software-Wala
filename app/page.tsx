b1ce4acdf74783600e1861cadb0b7af440b389df
abubkar141 <abubakargamerz1415@gmail.com>
2024-09-30 19:21:58 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+const page = () =>{
+ return (
+  <div>
+    <h1 className="text-center font-black mt-11 text-5xl">Hello&apos;World!</h1>
+    <p className="text-center font-bold">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, aliquam.</p>
+    <center>
+      <button className="bg-yellow-300 m-3 mt-3 p-2 rounded-md cursor-pointer hover:bg-yellow-400 transition">Learn More</button>
+    </center>
+  </div>
+)
+}
+export default page
